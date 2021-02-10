6b2e251ddb551aca6c7ecd3b878b04cdff11fe70
MariaZyryanova72 <mariyazz@inbox.ru>
2021-02-10 18:55:39 +0500

    Menu class? button - new lesson

--- app_window/const.py
+++ app_window/const.py
@@ -2,3 +2,4 @@ from os import path
 
 
 PATH_SPLASH_SCREEN = path.join('data', 'image', 'background', 'заставка.png')
+PATH_MAIN_MENU = path.join('data', 'image', 'background', 'main_background_app.jpg')
--- app_window/main.py
+++ app_window/main.py
@@ -5,8 +5,8 @@ import time
 
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSplashScreen, QDesktopWidget
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplashScreen, QDesktopWidget, QLabel, QWidget, QPushButton
 
 from const import *
 
@@ -16,17 +16,21 @@ class Normalize:
         self.init_width = 1920
         self.init_height = 1080
 
-        self.window_width_now = width
-        self.window_height_now = height
+        self.window_width = width
+        self.window_height = height
 
     def normal_xy(self, x, y):
-        return int(self.window_width_now / self.init_width * x), int(self.window_height_now / self.init_height * y)
+        return int(self.window_width / self.init_width * x), int(self.window_height / self.init_height * y)
 
-    def normal_prop_xy(self, x, y):
-        return int(self.window_width_now / self.init_width * x), int(self.window_width_now / self.init_width * y)
+    def normal_proportion(self, x, y):
+        return int(self.window_width / self.init_width * x),\
+               int(self.window_width / self.init_width * y)
 
     def normal_font(self, font):
-        return str(int(self.window_width_now / self.init_width * font))
+        if self.window_width / self.init_width > self.window_height / self.init_height:
+            return str(int(self.window_width / self.init_width * font))
+        else:
+            return str(int(self.window_height / self.init_height * font))
 
 
 class MainWindow(QMainWindow):
@@ -41,15 +45,60 @@ class MainWindow(QMainWindow):
 
         self.normal = Normalize(self.geometry.width(), self.geometry.height())
 
+        self.background = QLabel(self)
+
         self.initUI()
 
     def initUI(self):
         # Заставка
-        splash = QSplashScreen(QtGui.QPixmap(PATH_SPLASH_SCREEN).scaled(*self.normal.normal_prop_xy(1544, 900)),
+        splash = QSplashScreen(QtGui.QPixmap(PATH_SPLASH_SCREEN).scaled(*self.normal.normal_proportion(1544, 900)),
                                QtCore.Qt.WindowStaysOnTopHint)
         splash.show()
         time.sleep(1)
         splash.close()
+
+        pixmap = QPixmap(PATH_MAIN_MENU)
+        self.background.setPixmap(pixmap)
+        self.background.resize(self.geometry.width(), self.geometry.width())
+        self.background.setScaledContents(True)
+
+        menu = Menu(self)
+        menu.resize(self.geometry.width(), self.geometry.height())
+        menu.show()
+
+
+class Menu(QWidget):
+    def __init__(self, mainwindow):
+        super().__init__(mainwindow)
+        self.normal = mainwindow.normal
+
+        self.btn_new_lesson = QPushButton("Новый урок", self)
+        self.initUI()
+
+    def initUI(self):
+        # --------------------------
+        #       Кнопки меню
+        # --------------------------
+
+        # Кнопка создания нового урока
+        self.btn_new_lesson.resize(*self.normal.normal_xy(200, 50))
+        self.btn_new_lesson.move(*self.normal.normal_xy(175, 200))
+        # 548490 - темный голубой  76b7c7 - светлый голубой
+        self.btn_new_lesson.setStyleSheet(
+            '.QPushButton {'
+            'background-color: #76b7c7;'
+            'border-style: outset;'
+            'border-width: 2px;'
+            'border-radius: 10px;'
+            'border-color: beige;'
+            'font: bold ' + self.normal.normal_font(17) + 'px;'
+                                                          'min-width: 10em;'
+                                                          'padding: 6px;'
+                                                          '}'
+                                                          '.QPushButton:hover {'
+                                                          'background-color: #548490;'
+                                                          'border-style: inset;'
+                                                          '}')
 
 
 if __name__ == '__main__':
